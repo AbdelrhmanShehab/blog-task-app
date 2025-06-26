d9559c7c1fb26c8f5bcc209f7740885341173693
unknown <abdlrhman.shihab@gmail.com>
2025-06-26 16:25:05 +0300

    setting the structure of my project

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface BlogCardProps {
+  title: string;
+  author: string;
+  date: string;
+  excerpt: string;
+  onClick?: () => void;
+}
+
+export default function BlogCard() {
+  return <></>;
+}
